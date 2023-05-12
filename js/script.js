91675ca72e5f98a63905c2fd8d0da8d1959fcf9c
LukasJ00 <lukas@janfelt.se>
2023-05-12 14:39:31 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const playerScore = 0;
+const computerScore = 0; 
+const gameStarted = false; 
+
+function startGame(){
+    playerName = document.getElementById('name-input').value;
+
+ console.log(playerName)
+}
